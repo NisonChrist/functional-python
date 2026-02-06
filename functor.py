46015ca14dbb54963eca8ed25d7c090d062a5e5f
Ziv <nison1225@qq.com>
2026-02-06 16:29:30 +0800

    Wrap example usage in main guard

--- functor.py
+++ functor.py
@@ -13,8 +13,13 @@ class Functor:
         return f"Functor({self._value})"
 
 
-print(Functor.of(2).map(lambda x: x + 1))  # Functor(3)
-print(Functor.of(-1).map(lambda x: x + 1))  # Functor(0)
-print(Functor.of(0).map(lambda x: x + 1))  # Functor(1)
-print(Functor.of(99).map(lambda x: x + 1))  # Functor(100)
-print(Functor.of(99).map(lambda x: x + 1).map(lambda x: x * x))  # Functor(10000)
+def main():
+    print(Functor.of(2).map(lambda x: x + 1))  # Functor(3)
+    print(Functor.of(-1).map(lambda x: x + 1))  # Functor(0)
+    print(Functor.of(0).map(lambda x: x + 1))  # Functor(1)
+    print(Functor.of(99).map(lambda x: x + 1))  # Functor(100)
+    print(Functor.of(99).map(lambda x: x + 1).map(lambda x: x * x))  # Functor(10000)
+
+
+if __name__ == "__main__":
+    main()
